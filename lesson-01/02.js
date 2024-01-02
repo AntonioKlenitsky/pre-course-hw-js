a83d6478f237f4076df9e31b860278cbdc760355
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-01-02 21:17:13 +0300

    fx

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -5,12 +5,11 @@ let number1 = Math.floor(Math.random() * 100)
 С каждой перезагрузкой страницы у тебя будет новое число.​ */
 
 let number1 = Math.floor(Math.random() * 100)
-function checkOfNumber(){
+function checkOfNumber(number1){
     if (number1%2===0){
         return number1+":Это четное число";
     } else  {
         return number1+":Это нечетное число";
     }
 }
-
 console.log(checkOfNumber());

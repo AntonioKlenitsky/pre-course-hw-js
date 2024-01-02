4575cdfa12fa80a3d814beffdaf5054822cb3834
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-01-02 22:21:28 +0300

    fx

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,6 @@
 let number1 = Math.floor(Math.random() * 100)
 function checkOfNumber(number1){
-  if (number1===0){
-    return 0;
-  } else if (number1%2===0){
+    if (number1%2===0){
         return number1+ ":Это четное число";
     } else  {
         return number1+ ":Это нечетное число";

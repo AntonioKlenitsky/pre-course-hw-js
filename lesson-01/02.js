a682030a7f85db73afc21a74c1bbb67362d46ea2
Antonio Klenitsky <a_klenitsky@mail.ru>
2024-01-02 21:49:10 +0300

    fx

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -9,9 +9,9 @@ function checkOfNumber(number1){
   if (number1===0){
     return 0;
   } else if (number1%2===0){
-        return number1+":Это четное число";
+        return number1+ ":Это четное число";
     } else  {
-        return number1+":Это нечетное число";
+        return number1+ ":Это нечетное число";
     }
 }
 console.log(checkOfNumber(number1));
